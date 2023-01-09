d9416bb05a1f6e7f8b06a2b03470d8b68a41365c
Andrii Doroshenko <mapleukraine@gmail.com>
2023-01-09 20:19:15 +0200

    directory category made

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,23 @@
+import './category-item.components.scss';
+
+const CategoryItem = (props) => {
+  const {
+    category: { title, imageUrl },
+  } = props;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
