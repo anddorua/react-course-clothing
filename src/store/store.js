de7363acb54bc6c68dc7783d4381e1f7e075991e
Andrii Doroshenko <mapleukraine@gmail.com>
2023-01-30 20:43:18 +0200

    redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,32 @@
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { compose, applyMiddleware, createStore } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: "user",
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
